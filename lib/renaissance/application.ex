07841e1730848e80a7a0e6b277520787d06b1080
Jason Wodicka <jason@wodicka.net>
2021-06-18 12:49:16 -0700

    Initial commit of Phoenix generated application

---
+++ lib/renaissance/application.ex
@@ -0,0 +1,32 @@
+defmodule Renaissance.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      RenaissanceWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Renaissance.PubSub},
+      # Start the Endpoint (http/https)
+      RenaissanceWeb.Endpoint
+      # Start a worker by calling: Renaissance.Worker.start_link(arg)
+      # {Renaissance.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Renaissance.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    RenaissanceWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
