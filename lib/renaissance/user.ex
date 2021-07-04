a6382f1770df45e7909cfe62b98f1d3723739047
Jason Wodicka <jason@wodicka.net>
2021-07-03 21:31:53 -0700

    attach auth0 logins to DB records

--- lib/renaissance/user.ex
+++ lib/renaissance/user.ex
@@ -2,18 +2,39 @@ defmodule Renaissance.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:authid, :string, autogenerate: false}
   schema "users" do
-    field :authid, :string
+    # field :authid, :string
     field :name, :string
     field :sysadmin, :boolean, default: false
 
     timestamps()
   end
 
+  @spec find_or_create(String.t, String.t) :: {:ok, User} | {:error, term}
+  def find_or_create(authid, name) do
+    case find_by_authid(authid) do
+      nil -> create(%{authid: authid, name: name})
+      user -> {:ok, user}
+    end
+  end
+
+  def create(attrs) do
+    %Renaissance.User{}
+    |> changeset(attrs)
+    |> Renaissance.Repo.insert()
+  end
+
+  @spec find_by_authid(any) :: any
+  def find_by_authid(authid) do
+    IO.puts(authid)
+    Renaissance.Repo.get(Renaissance.User, authid)
+  end
+
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:id, :name, :sysadmin])
-    |> validate_required([:id, :name, :sysadmin])
+    |> cast(attrs, [:authid, :name, :sysadmin])
+    |> validate_required([:authid, :name])
   end
 end
--- lib/renaissance_web/controllers/auth_controller.ex
+++ lib/renaissance_web/controllers/auth_controller.ex
@@ -20,9 +20,16 @@ defmodule RenaissanceWeb.AuthController do
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
-    conn
-#    |> put_flash(:info, "Hey, I know who you are!")
-    |> put_flash(:info, auth.uid)
-    |> redirect(to: "/")
+    case Renaissance.User.find_or_create(auth.uid, auth.info.name) do
+      {:ok, user} ->
+        conn
+        # |> put_flash(:info, "Hey, I know who you are!")
+        |> put_flash(:info, user.name)
+        |> redirect(to: "/")
+      {:error, reason} ->
+        conn
+        |> put_flash(:error, reason)
+        |> redirect(to: "/")
+    end
   end
 end
