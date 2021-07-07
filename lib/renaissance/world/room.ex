60246bfde4e8547a1920d77e4fef5934316610ce
Jason Wodicka <jason@wodicka.net>
2021-07-06 18:05:33 -0700

    big streaming changes
    * lots of styling
    * multiline descriptions for rooms
    * some refactoring of templates
    
    Co-authored-by: kaivex <kaivex@users.noreply.github.com>

--- lib/renaissance/world/room.ex
+++ lib/renaissance/world/room.ex
@@ -13,10 +13,16 @@ defmodule Renaissance.World.Room do
     timestamps()
   end
 
+  @spec unicodify_newlines(binary) :: binary
+  def unicodify_newlines(str) do
+    Regex.replace(~r/[\r\n]+/, str, "\u2029")
+  end
+
   @doc false
   def changeset(room, attrs) do
     room
     |> cast(attrs, [:description, :name])
     |> validate_required([:description, :name])
+    |> update_change(:description, &unicodify_newlines/1)
   end
 end
